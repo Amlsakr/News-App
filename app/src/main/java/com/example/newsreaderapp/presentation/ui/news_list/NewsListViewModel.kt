d4a57d3f971859ba528570b43c87770331a48edd
OTVENTURES\Amal.Sakr <amal.sakr@linkdev.com>
2024-08-20 23:41:33 +0300

    implement news details

--- app/src/main/java/com/example/newsreaderapp/presentation/ui/news_list/NewsListViewModel.kt
+++ app/src/main/java/com/example/newsreaderapp/presentation/ui/news_list/NewsListViewModel.kt
@@ -1,16 +1,15 @@
 package com.example.newsreaderapp.presentation.ui.news_list
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.example.newsreaderapp.common.utill.Resource
 import com.example.newsreaderapp.domain.model.Article
 import com.example.newsreaderapp.domain.useCase.GetNewsUseCase
 import com.example.newsreaderapp.presentation.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,7 +20,10 @@ class NewsListViewModel @Inject constructor(val getNewsUseCase: GetNewsUseCase) 
     ) {
     val newsPagingDataFlow: Flow<PagingData<Article>> = getNewsUseCase()
         .cachedIn(viewModelScope)
-//    init {
+    private val _selectedCategory = MutableStateFlow<String>("")
+    val selectedCategory: StateFlow<String> = _selectedCategory
+
+    //    init {
 //        viewModelScope.launch {
 //            getNewsUseCase.invoke().collect { result ->
 //                sendEvent(
@@ -53,5 +55,8 @@ class NewsListViewModel @Inject constructor(val getNewsUseCase: GetNewsUseCase) 
 //
 //        }
 //    }
-
+    fun onOptionSelected(option: String) {
+        _selectedCategory.value = option
+        //   makeApiRequest(option)
+    }
 }
